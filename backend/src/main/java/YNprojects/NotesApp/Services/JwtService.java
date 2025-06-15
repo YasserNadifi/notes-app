dce402418d9ea2a76005efe430892b8548d04366
YasserNadifi <yassernadifi2003@gmail.com>
2025-06-15 01:25:25 +0100

    .

--- backend/src/main/java/YNprojects/NotesApp/Services/JwtService.java
+++ backend/src/main/java/YNprojects/NotesApp/Services/JwtService.java
@@ -7,6 +7,7 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
@@ -16,7 +17,8 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    public final String SECRET_KEY="3fa0810a85f348ca1a81280e68fa74f0b41480dc4de9fb7c27416bc9398a24ca64d50e380fa04936b8770af6f84554a161d274a751adf40b6d590fc7b25934dafc0289a51a267bde2c64323b472b69b8da136abcf24c0e3ec93a7243f6c4b6c43bf45b61203b81e748b2d70192fe609d13a9bf386e13377ed3fdd3b45ee31820f122534e89c0e0761f83e162b8cc2be2e688385f058d32d0b0f9a7de9fa598c944a385423ce99ac03435677e0b93fa0ed0d63dffcf43e1e95af32ee518ec1926e0027ec54199a9d17cf4a84a26f3f956ca722228f68e53a455cce01e55af81ad6aafb0ec6ace663b47562493bb18580a887d1aabc141c32ec320d97856ef90bb";
+    @Value("${jwt.secret}")
+    public String SECRET_KEY;
 
     public boolean isValid(String token, UserDetails user) {
         String username=extractUsername(token) ;
